1978a072e0be754171f60533f7954f1e113c5d1c
Pavel <76093403+pavelkrasulin@users.noreply.github.com>
2021-05-21 12:09:48 +0400

    Подключает js к html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// .chrome styling Vanilla JS
+// .chrome styling
 
-document.getElementById("myinput").oninput = function() {
-  this.style.background = 'linear-gradient(to right, #C29974 0%, #C29974 ' + this.value + '%, #666 ' + this.value + '%, #666 100%)'
-};
+// document.getElementById("input-range").oninput = function() {
+//  this.style.background = 'linear-gradient(to right, #C29974 0%, #C29974 ' + this.value + '%, #666 ' + this.value + '%, #666 100%)'
+//};
+
+// .chrome
+
+//$( '.chrome input' ).on( 'input', function( ) {
+//  $( this ).css( 'background', 'linear-gradient(to right, green, #005555 ' + this.value + '%, blue)' );
+//} );
+
+// .chrome styling
+
+$( '.filters-col input::-webkit-slider-runnable-track' ).on( 'input::-webkit-slider-runnable-track', function( ) {
+  $( this ).css( 'background', 'linear-gradient(to right, #C29974 0%, #C29974 ' + this.value + '%, #666 ' + this.value + '%, #666 100%)' );
+} );
